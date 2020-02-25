3caf9a110275d68d3b1a73a2dabf0a960f5f279a
Nicola Biada <nicola@biada.it>
2020-02-25 18:21:33 +0100

    Add project files.

---
+++ README.md
@@ -0,0 +1,42 @@
+﻿# Blazor Lightboard
+## The Blazor Lightweight Dashboard Template
+
+This project is a template for a lightweight responsive dashboard in Blazor.\
+It's based on the _Blazor-Dashboard_ project available via github.
+
+I've developed this admin template for my .net core projects.\
+The aim was to use Blazor (C#), Boostrap and _the smallest amount_ of Javascript for development in server side mode.
+
+I've implemented a simple **State Manager** that you can expand as you like.\
+Try to fill the input text in the _Home_ or to increment the counter, you'll see the updated properties on the right card.\
+All the updates are made via State Manager methods.
+
+The value of the _Counter_ property is persisted via localStorage.\
+I've used the [Blazor Storage Library](https://github.com/cloudcrate/BlazorStorage) for localStorage support.
+
+This template is available under the MIT License.
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+## Usage
+
+1. Clone or Download [nbiada/BlazorLightboard](https://github.com/nbiada/BlazorLightboard)
+2. open a Command Prompt or a shell
+3. change directory to the BlazorLightboard subfolder
+4. ``dotnet run``
+5. Access the dashboard through https://localhost:5001/
+
+
+## See it in Action
+
+### Home
+![Home](https://user-images.githubusercontent.com/4179320/75267717-e1cb7480-57f6-11ea-8c06-aefc0b6ed335.png?s=400)
+
+### Counter
+![Counter](https://user-images.githubusercontent.com/4179320/75267777-f7d93500-57f6-11ea-8568-a5a6cc6c39a2.png)
+
+### Weather
+![Weather](https://user-images.githubusercontent.com/4179320/75267795-fad42580-57f6-11ea-927e-3b51cd638190.png)
+
+### Home Mobile
+![Home-Mobile](https://user-images.githubusercontent.com/4179320/75267808-ff98d980-57f6-11ea-87ea-82907fa211fc.png)
---
+++ UXState.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorLightboard
+{
+    public class UXState
+    {
+        public bool NavBarDisplay { get; private set; }
+        public bool SlideInDisplay { get; private set; }
+        public string SlideInClass { get; private set; }
+        public string ReverseName { get; private set; }
+        public string FirstName { get; private set; }
+        public string DefaultFirstName { get; private set; } = "Who?";
+        private int _counter;
+        public int Counter
+        {
+            get
+            {
+                return _counter;
+            }
+            set
+            {
+                _counter = value;
+                NotifyStateChanged();
+            }
+        }
+
+
+        public event Action OnChange;
+
+
+        private string _name;
+
+        public UXState()
+        {
+            NavBarDisplay = true;
+        }
+
+        public void SetNavBarDisplay(bool visible)
+        {
+            NavBarDisplay = visible;
+            NotifyStateChanged();
+        }
+        public void SetSlideInDisplay(bool visible)
+        {
+            SlideInDisplay = visible;
+            if (visible)
+            {
+                SlideInClass = "slidebar-open";
+            }
+            else
+            {
+                SlideInClass = string.Empty;
+            }
+            NotifyStateChanged();
+        }
+
+        public void ToggleSlideIn()
+        {
+            SetSlideInDisplay(!SlideInDisplay);
+        }
+        public void ToggleNavBar()
+        {
+            SetNavBarDisplay(!NavBarDisplay);
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                SetReverseName(value);
+                SetFirstName(value);
+                NotifyStateChanged();
+            }
+        }
+
+        public void SetReverseName(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                ReverseName = ReverseStringDirect(name);
+            }
+            else
+            {
+                ReverseName = "";
+            }
+        }
+        public void SetFirstName(string name)
+        {
+            if (string.IsNullOrEmpty(name) || name.Equals(DefaultFirstName))
+            {
+                FirstName = DefaultFirstName;
+            }
+            else
+            {
+                FirstName = GetFirstWord(name);
+            }
+        }
+
+        private static string ReverseStringDirect(string s)
+        {
+            char[] array = new char[s.Length];
+            int forward = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                array[forward++] = s[i];
+            }
+            return new string(array);
+        }
+        private static string GetFirstWord(string s)
+        {
+            return String.Join("", s.TakeWhile(x => Char.IsLetter(x)));
+        }
+
+        public int KeyValueToInt(object keyValue)
+        {
+            if (keyValue == null)
+            {
+                return 0;
+            }
+            try
+            {
+                var strValue = keyValue.ToString();
+                return Convert.ToInt32(strValue);
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+        public string KeyValueToString(object keyValue)
+        {
+            if (keyValue == null)
+            {
+                return "";
+            }
+            try
+            {
+                var strValue = keyValue.ToString();
+                return strValue;
+            }
+            catch
+            {
+                return "";
+            }
+        }
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
