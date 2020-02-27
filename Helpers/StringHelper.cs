40c0a553cb3ed34e05bb4ecb50af7311f91f4a20
Nicola Biada <nicola@biada.it>
2020-02-27 11:37:02 +0100

    Fix footer OS string too long

---
+++ Helpers/StringHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorLightboard.Helpers
+{
+    public static class StringHelper
+    {
+        public static string Truncate(this string text, int length, bool useEllipsis, string ellipsis = "...", bool keepFullWordAtEnd = false)
+        {
+            if (string.IsNullOrEmpty(text)) return string.Empty;
+
+            if (text.Length < length) return text;
+
+            text = text.Substring(0, length);
+
+            if (keepFullWordAtEnd)
+            {
+                text = text.Substring(0, text.LastIndexOf(' '));
+            }
+
+            if (!useEllipsis) return text;
+
+            return text + ellipsis;
+        }
+    }
+}
